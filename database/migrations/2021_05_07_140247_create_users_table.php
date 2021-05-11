3b507926d95eb7999a189ad5c18c0a9c29a8e559
Atakan ATICI <atakanatici0@gmail.com>
2021-05-11 22:48:34 +0300

    migrations

---
+++ database/migrations/2021_05_07_140247_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('sicil_no')->nullable();
+            $table->integer('author_no')->nullable();
+            $table->string('name', 100)->nullable();
+            $table->string('tel_no',19)->nullable();
+            $table->string('adres')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

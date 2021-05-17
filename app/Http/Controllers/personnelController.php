d4764898849d9e27f9f44df1d7eb3b174a703aae
Atakan ATICI <atakanatici0@gmail.com>
2021-05-17 14:53:35 +0300

    personel kayıt kontrol

--- app/Http/Controllers/personnelController.php
+++ app/Http/Controllers/personnelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class personnelController extends Controller
 {
@@ -66,17 +67,37 @@ class personnelController extends Controller
             ]
         );
 
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->tel_no = $request->tel_no;
-        $user->prsnl_no = $request->prsnl_no;
-        $user->sicil_no = $request->sicil_no;
-        $user->password = Hash::make($request->password);
-        $user->author_no = $request->author_no;
-        $user->adress = $request->adress;
-        $user->save();
+        $email = $request->email;
+        $findEmail = User::where('email', '=', $email)->first();
 
-        return redirect()->route('show.personnelList');
+        $prsnlNo = $request->prsnl_no;
+        $findPrsnlNo = User::where('prsnl_no', '=', $prsnlNo)->first();
+
+        if ($findEmail != null)
+        {
+            Alert::warning('Hata!', 'Bu eposta başka bir kullanıcı için tanımlı.');
+            return redirect()->route('show.add.personnel');
+        }
+        else if($findPrsnlNo != null)
+        {
+            Alert::warning('Hata!', 'Bu personel numarası başka bir kullanıcı tarafından kullanılıyor.');
+            return redirect()->route('show.add.personnel');
+        }
+        else
+        {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->tel_no = $request->tel_no;
+            $user->prsnl_no = $request->prsnl_no;
+            $user->sicil_no = $request->sicil_no;
+            $user->password = Hash::make($request->password);
+            $user->author_no = $request->author_no;
+            $user->adress = $request->adress;
+            $user->save();
+
+            return redirect()->route('show.personnelList');
+        }
+     
     }
 }
